d204942f5fae33ecace9c86998fe6d3af27c61a8
Osmar Hernández <osmarhernandezs@gmail.com>
2019-04-10 16:38:52 -0600

    OnBoarding process added

---
+++ MoneyTogether/Custom Elements/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  MoneyTogether
+//
+//  Created by Osmar Hernández on 10/04/19.
+//  Copyright © 2019 pastdue. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
