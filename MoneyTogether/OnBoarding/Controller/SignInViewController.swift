065357e35bf7d46de9ead03f351838befe397a3f
Jaime Solís <jaimesolissoto@me.com>
2019-04-26 16:09:25 -0600

    Views added to login

---
+++ MoneyTogether/OnBoarding/Controller/SignInViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.swift
+//  MoneyTogether
+//
+//  Created by Jaime Solís on 4/26/19.
+//  Copyright © 2019 pastdue. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+}
