08f6d1c8b1a165aad64d4c0f8c862a854ea47719
Antonio Spinola <antoniospinola@Antonios-MacBook-Pro.local>
2019-05-03 18:09:18 -0600

    vistas: SendMoney, RequestMoney y Loans

---
+++ MoneyTogether/Home/Controller/CheckBoxVC.swift
@@ -0,0 +1,9 @@
+//
+//  CheckBoxVC.swift
+//  MoneyTogether
+//
+//  Created by Antonio Spinola Flores on 5/3/19.
+//  Copyright © 2019 pastdue. All rights reserved.
+//
+
+import Foundation
---
+++ MoneyTogether/Home/Controller/CheckBoxViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CheckBoxViewController.swift
+//  MoneyTogether
+//
+//  Created by Antonio Spinola Flores on 5/3/19.
+//  Copyright © 2019 pastdue. All rights reserved.
+//
+
+import UIKit
+
+
+class CheckBoxViewController: UIViewController {
+
+    @IBAction func TermsAnfConditions(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
