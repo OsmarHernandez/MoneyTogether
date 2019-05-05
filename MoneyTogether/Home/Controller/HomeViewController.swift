fa1ee5ba4d461ec6fe21be12f888ba8ed59f49b7
Osmar Hernández <osmarhernandezs@gmail.com>
2019-05-04 20:37:19 -0600

    Added Home View Controller

---
+++ MoneyTogether/Home/Controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  MoneyTogether
+//
+//  Created by Osmar Hernández on 04/05/19.
+//  Copyright © 2019 pastdue. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
