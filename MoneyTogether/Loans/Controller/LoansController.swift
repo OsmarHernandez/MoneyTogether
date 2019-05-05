33448021e438df80d3573847f5b4a1a674e407e0
Osmar Hernández <osmarhernandezs@gmail.com>
2019-05-05 02:35:57 -0600

    View updated

--- MoneyTogether/Loans/Controller/LoansController.swift
+++ MoneyTogether/Loans/Controller/LoansController.swift
@@ -21,14 +21,16 @@ class LoansController: UIViewController {
         ("Rosas Regina", 790.00)
     ]
 
-    let details: [(key: String, title: String, amount: String, remaining: String, value: String)] = [
-        ("345634562","Despensa Jaime", "12", "11", "01/06/19"), ("234667234","Jaime", "111", "121", "13/06/19"), ("xxxxxxx","xxxxxxx", "xxxxxxx", "xxxxxxx", "xxxxxxx")
+    let details: [(key: String, title: String, amount: Double, remaining: Double, value: String)] = [
+        ("345634562","Despensa Jaime", 1_300.00, 4_210.00, "01-06-2019"),
+        ("234667234","Jaime", 5_200.00, 6_210.00, "13-06-2019"),
+        ("432402401","Osmar", 5_200.00, 6_210.00, "12-04-2019")
     ]
     
-    let lendedDetails: [(key: String, title: String, amount: String, remaining: String, value: String)] = [
-        ("D345634562","Ddespensa Jaime", "12112", "1121", "01/06/10"), ("D234667234","DJaime", "D111", "D121", "D13/08/18")
+    let lendedDetails: [(key: String, title: String, amount: Double, remaining: Double, value: String)] = [
+        ("D345634562","Ddespensa Jaime", 1_210.00, 980.00, "01-06-2019"),
+        ("D234667234","DJaime", 790.00, 3_230.00, "13-08-2019")
     ]
-    
     
     @IBOutlet weak var loanID: UILabel!
     
@@ -37,13 +39,30 @@ class LoansController: UIViewController {
     @IBOutlet weak var detailsTitle: UILabel!
     @IBOutlet weak var nextPayment: UILabel!
     
-    
-    
     @IBOutlet weak var borrowedCollectionView: UICollectionView!
     @IBOutlet weak var lendedCollectionView: UICollectionView!
     
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .currency
+        
+        return nf
+    }()
+    
     override func viewDidLoad()  {
         super.viewDidLoad()
+        
+        initialSetup()
+    }
+    
+    func initialSetup() {
+        let loan = details[0]
+        
+        loanID.text =  loan.key
+        detailsTitle.text =  loan.title
+        amountReq.text =  numberFormatter.string(from: NSNumber(value: loan.amount))
+        amountRem.text =  numberFormatter.string(from: NSNumber(value: loan.remaining))
+        nextPayment.text =  loan.value
     }
 }
 
@@ -52,20 +71,13 @@ extension LoansController: UICollectionViewDelegate, UICollectionViewDataSource 
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-         if collectionView == borrowedCollectionView {
-        loanID.text =  details[indexPath.row].key
-        detailsTitle.text =  details[indexPath.row].title
-        amountReq.text =  details[indexPath.row].amount
-        amountRem.text =  details[indexPath.row].remaining
-        nextPayment.text =  details[indexPath.row].value
-         }else{
-        loanID.text =  lendedDetails[indexPath.row].key
-        detailsTitle.text =  lendedDetails[indexPath.row].title
-        amountReq.text =  lendedDetails[indexPath.row].amount
-        amountRem.text =  lendedDetails[indexPath.row].remaining
-        nextPayment.text =  lendedDetails[indexPath.row].value
-            
-        }
+        let loan = collectionView == borrowedCollectionView ? details[indexPath.row] : lendedDetails[indexPath.row]
+        
+        loanID.text =  loan.key
+        detailsTitle.text =  loan.title
+        amountReq.text =  numberFormatter.string(from: NSNumber(value: loan.amount))
+        amountRem.text =  numberFormatter.string(from: NSNumber(value: loan.remaining))
+        nextPayment.text =  loan.value
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -87,7 +99,7 @@ extension LoansController: UICollectionViewDelegate, UICollectionViewDataSource 
             let borrowedCell = borrowedCollectionView.dequeueReusableCell(withReuseIdentifier: "borrowedCell", for: indexPath) as! LoanCollectionViewCell
             
             borrowedCell.loanNameLabel.text = borrowed[indexPath.row].key
-            borrowedCell.amountLabel.text = "$\(borrowed[indexPath.row].value)"
+            borrowedCell.amountLabel.text = numberFormatter.string(from: NSNumber(value: borrowed[indexPath.row].value))
             
             return borrowedCell
         }
@@ -95,7 +107,7 @@ extension LoansController: UICollectionViewDelegate, UICollectionViewDataSource 
         let lendedCell = lendedCollectionView.dequeueReusableCell(withReuseIdentifier: "lendedCell", for: indexPath) as! LoanCollectionViewCell
         
         lendedCell.loanNameLabel.text = lended[indexPath.row].key
-        lendedCell.amountLabel.text = "$\(lended[indexPath.row].value)"
+        lendedCell.amountLabel.text = numberFormatter.string(from: NSNumber(value: lended[indexPath.row].value))
         
         return lendedCell
     }
